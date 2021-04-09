862a04b2483e3bbaac4626bf059bfd96d69786e7
Beka Stiling <stilir@uw.edu>
2021-04-09 10:21:54 -0700

    Pausing until next week.

--- Test_Script.R
+++ Test_Script.R
@@ -1,1 +1,18 @@
 ## New script, shiny tutorial
+
+#Attach necessary files
+library(tidyverse)
+library(shiny)
+library(shinythemes)
+
+
+# Create the user interface:
+ui <- fluidPage()
+
+# Create the server function:
+server <- function(input, output) {}
+
+# Combine them into an app:
+shinyApp(ui = ui, server = server)
+
+
